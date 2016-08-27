9fa51365e7dcc152ade06b92028641db89fe24ac
kdontula <kdontula@hawk.iit.edu>
2016-08-27 18:14:19 -0500

    looks good still need some ux changes

--- app/src/main/java/com/kaushal/toolkit/level/LevelActivity.java
+++ app/src/main/java/com/kaushal/toolkit/level/LevelActivity.java
@@ -1,14 +1,10 @@
 package com.kaushal.toolkit.level;
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Surface;
-import android.view.WindowManager;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.kaushal.toolkit.R;
 
@@ -18,9 +14,11 @@ public class LevelActivity extends AppCompatActivity implements LevelListener {
 
     private static Context context;
     public DecimalFormat df = new DecimalFormat("#.#");
-    private float lastXValue;
-    private float lastYValue;
-    ImageView imgAnimation;
+    private float lastXValue = 0f;
+    private float lastYValue = 0f;
+    ImageView imgAnimationX;
+    ImageView imgAnimationY;
+    ImageView imgAnimationXY;
     TranslateAnimation animation;
     String currentOrientation;
 
@@ -29,15 +27,6 @@ public class LevelActivity extends AppCompatActivity implements LevelListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level_main);
         context = this;
-        currentOrientation = getRotation(context);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        lastXValue = 0f;
-        lastYValue = 0f;
-        super.onConfigurationChanged(newConfig);
-        currentOrientation = getRotation(context);
     }
 
     protected void onResume() {
@@ -63,48 +52,36 @@ public class LevelActivity extends AppCompatActivity implements LevelListener {
     }
     
     public void onAccelerationChanged(float x, float y, float z) {
-        float animMultiplier = 80f;
+        float animMultiplier = 50f;
         float animX = x * animMultiplier;
-        float animY = y * animMultiplier;
-        TextView value = (TextView) findViewById(R.id.value);
-        imgAnimation = (ImageView) findViewById(R.id.imageView);
-        assert value != null;
-        if (currentOrientation == "portrait"){
-            imgAnimation.startAnimation(bubbleAnimation(animX, 0f));
-            value.setText(String.valueOf(df.format(x)));
-            lastXValue = animX;
-            lastYValue= 0f;
-        }
-        else if (currentOrientation == "landscape" || currentOrientation == "reverse landscape"){
-            imgAnimation.startAnimation(bubbleAnimation(animY, 0f));
-            value.setText(String.valueOf(df.format(y)));
-            lastXValue = animY;
-            lastYValue= 0f;
-        }
-        else{
-            value.setText("Orientation Error!! Please Restart application.");
-        }
+        float animY = -y * animMultiplier;
+
+
+//        TextView value = (TextView) findViewById(R.id.value);
+        imgAnimationX = (ImageView) findViewById(R.id.imageViewX);
+        imgAnimationY = (ImageView) findViewById(R.id.imageViewY);
+        imgAnimationXY = (ImageView) findViewById(R.id.imageViewXY);
+//        assert value != null;
+
+        imgAnimationX.startAnimation(bubbleAnimation(lastXValue, animX, 0f, 0f));
+//        value.setText(String.valueOf(df.format(x)));
+
+
+        imgAnimationY.startAnimation(bubbleAnimation(0f, 0f, lastYValue, animY));
+//        value.setText(String.valueOf(df.format(y)));
+
+        imgAnimationXY.startAnimation(bubbleAnimation(lastXValue, animX, lastYValue, animY));
+//        value.setText(String.valueOf(df.format(y)));
+        lastXValue = animX;
+        lastYValue= animY;
+
     }
 
-    private TranslateAnimation bubbleAnimation(float x, float y) {
-        animation = new TranslateAnimation(lastXValue, x, lastYValue, y);
+    private TranslateAnimation bubbleAnimation(float lX, float x, float lY, float y) {
+        animation = new TranslateAnimation(lX, x, lY, y);
         animation.setDuration(5000);
         animation.setFillAfter(true);
         return animation;
 
-    }
-
-    public String getRotation(Context context){
-        final int rotation = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                return "portrait";
-            case Surface.ROTATION_90:
-                return "landscape";
-            case Surface.ROTATION_180:
-                return "reverse portrait";
-            default:
-                return "reverse landscape";
-        }
     }
 }
